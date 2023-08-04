45fb9a5ffb2786b8141654cc592af1dd58af7cb0
macrespo42 <macrespo@student.42.fr>
2023-08-04 17:46:02 +0200

    feat: setup routing and global styles - setup routing of: root, error, BrowsingPage   - installation of react dom router - define global styles   - init colors, and font styles   - installing icons lib

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LandingPage from './components/LandingPage/LandingPage';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import BrowsingPage from './components/BrowsingPage/BrowsingPage';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/browsing',
+    element: <BrowsingPage />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
