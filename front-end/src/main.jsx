642584c35f654c4e2d5fdbb5c730d53c6d8b6ced
macrespo42 <macrespo@student.42.fr>
2023-08-28 18:44:26 +0200

    feat: integrate footer

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -8,6 +8,7 @@ import LandingPage from './components/LandingPage/LandingPage';
 import ErrorPage from './components/ErrorPage/ErrorPage';
 import BrowsingPage from './components/BrowsingPage/BrowsingPage';
 import './index.css'
+import Footer from './components/Footer/Footer';
 
 const router = createBrowserRouter([
   {
@@ -21,8 +22,11 @@ const router = createBrowserRouter([
   }
 ]);
 
+const currentPath = window.location.pathname;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    { currentPath !== '/' && <Footer />}
   </React.StrictMode>,
 )
