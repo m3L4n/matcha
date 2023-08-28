ae4374a32b63369c45a0062c721516508b914e5a
macrespo42 <macrespo@student.42.fr>
2023-08-28 18:51:21 +0200

    feat: add title to footer links

--- front-end/src/components/Footer/Footer.jsx
+++ front-end/src/components/Footer/Footer.jsx
@@ -5,9 +5,9 @@ export default function Footer() {
   return (
     <footer>
       <p>
-        <a href='https://github.com/m3L4n/matcha'><BsDiscord /></a> 
-        <a href='https://github.com/m3L4n/matcha'><BsGithub /></a> 
-        <a href='https://github.com/m3L4n/matcha'><BsTwitter /></a> 
+        <a href='https://github.com/m3L4n/matcha' title='discord'><BsDiscord /></a> 
+        <a href='https://github.com/m3L4n/matcha' title='github'><BsGithub /></a> 
+        <a href='https://github.com/m3L4n/matcha' title='twitter'><BsTwitter /></a> 
       </p>
     </footer>
   );
