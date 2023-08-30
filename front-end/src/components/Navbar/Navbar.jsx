1e5d1050ae2e9f520d63b075c3f1ebd8550a7829
macrespo42 <macrespo@student.42.fr>
2023-08-30 16:39:18 +0200

    feat: split main.jsx components

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -3,26 +3,26 @@ import './Navbar.scoped.css';
 import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
-  const [sidebar, setSidebar] = useState(false);
   const pages = ['match', 'profile', 'messages', 'disconnect'];
+  const [sidebar, setSidebar] = useState(false);
 
   const toggleSidebar = () => (setSidebar(!sidebar));
 
   return (
     <nav className={sidebar ? 'navbar navbar-deployed' : 'navbar' }>
       <a className={sidebar ? 'sidebar-toggle nav-open' : 'sidebar-toggle'} 
-         onClick={toggleSidebar}
+      onClick={toggleSidebar}
       >
-        <span className="burger menu-toggle-bar--top"></span>
-        <span className="burger menu-toggle-bar--middle"></span>
-        <span className="burger menu-toggle-bar--bottom"></span>
+      <span className="burger menu-toggle-bar--top"></span>
+      <span className="burger menu-toggle-bar--middle"></span>
+      <span className="burger menu-toggle-bar--bottom"></span>
       </a>
       <ul>
-        {pages.map((page) => (
-          <li key={pages.indexOf(page)}>
-            {sidebar && <NavLink className={`body`} to={page}>{page}</NavLink>}
-          </li>
-        ))}
+      {pages.map((page) => (
+        <li key={pages.indexOf(page)}>
+        {sidebar && <NavLink className={`body`} to={page}>{page}</NavLink>}
+        </li>
+      ))}
       </ul>
     </nav>
   );
