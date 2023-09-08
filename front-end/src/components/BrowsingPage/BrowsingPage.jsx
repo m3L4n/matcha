211af82ea12ba00367867cc6bba8e26947285cb2
macrespo42 <macrespo@student.42.fr>
2023-09-08 18:14:16 +0200

    feat: remove useless import from browsing page

--- front-end/src/components/BrowsingPage/BrowsingPage.jsx
+++ front-end/src/components/BrowsingPage/BrowsingPage.jsx
@@ -1,10 +1,7 @@
 import './BrowsingPage.scoped.css';
-import { BsFilter, BsSearch } from 'react-icons/bs'
-import { CgClose } from 'react-icons/cg';
 import dummy from './dummy_data.json'
 import Card from './Card/Card'
 import SearchBar from './SearchBar/SearchBar';
-import { useState } from 'react';
 
 export default function BrowsingPage() {
   const cards = dummy.map(user => <Card key={user.id} user={user} />)
