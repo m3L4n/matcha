23ab766226fa000c4c4f2876fcdae5d99622a210
mpochard <88907375+m3L4n@users.noreply.github.com>
2023-09-19 15:42:08 +0200

    push

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -13,6 +13,9 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider } from "./Context/AuthContext";
 import "./index.css";
+
+
+
 import { router } from "./Router/Router";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
