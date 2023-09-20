a6ca0819bb41fee0a21c20b68657b9f578d9b784
macrespo42 <macrespo@student.42.fr>
2023-09-20 16:25:40 +0200

    fix: complete user columns then fix seed - fix synchronous problem in seed loop - add age column to users

--- back-end/seed.js
+++ back-end/seed.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require("uuid");
 const db = require('./db/db');
 require("dotenv").config();
 
-async function generateUser() {
+const generateUser = () => {
   const random = Math.floor(Math.random() * 10);
   const firstName = faker.person.firstName();
   const lastName = faker.person.lastName();
@@ -15,22 +15,20 @@ async function generateUser() {
   const sexual_preference = random % 2 == 0 ? "male" : "female";
   const password = faker.string.alphanumeric();
   const description = faker.string.alphanumeric(200);
-  const rate_fame = Math.floor(Math.random() * 420);
+  const rate_fame = 1500 - Math.floor(Math.random() * 200)
+  const age = Math.floor(Math.random() * 60);
   const profile_picture = faker.image.avatar();
   const valided = true;
   const position = faker.location.nearbyGPSCoordinate();
   const query = "INSERT INTO \
-  users(id, username, email, firstName, gender, beverage, sexual_preference, lastName, password, description, rate_fame, position, profile_picture, valided) \
-  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)";
-  const params = [uuidv4(), username, email, firstName, gender, beverage, sexual_preference, lastName, password, description, rate_fame, position, profile_picture, valided];
-  try {
-    await db.query(query, params);
-  } catch (error) {
-    console.error(error)
-    throw error;
-  }
+  users(id, username, email, firstName, gender, beverage, sexual_preference, lastName, password, description, rate_fame, position, profile_picture, valided, age) \
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)";
+  const params = [uuidv4(), username, email, firstName, gender, beverage, sexual_preference, lastName, password, description, rate_fame, position, profile_picture, valided, age];
+  return db.query(query, params);
 }
 
 for (let i = 0; i < 42; i++) {
-  generateUser();
+  generateUser()
+    .then(() => console.log(`fake user ${i} inserted ✅`))
+    .catch(error => console.error(error))
 }
