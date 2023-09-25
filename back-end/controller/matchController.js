7aa2116d4711739ede694b2145f15ae214a03b77
macrespo42 <macrespo@student.42.fr>
2023-09-25 14:55:33 +0200

    feat: expose create like route

---
+++ back-end/controller/matchController.js
@@ -0,0 +1,15 @@
+const { checkAndChange } = require("../../modules/response");
+const { MatchModel } = require("../../models/MatchModel");
+
+class MatchController {
+  static create = async (req, res) => {
+    let requesterId = req.isAuth.id;
+    const receiverId = req.body.receiverId;
+    let match = MatchModel.create(requesterId, receiverId);
+    res.json(checkAndChange(match));
+  }
+}
+
+module.exports = {
+  MatchController,
+};
--- back-end/routes/match.js
+++ back-end/routes/match.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { isAuth } = require("../middlewares/userAuth");
+const { MatchController } = require("../controller/matchController");
 
-router.post("/create", isAuth, /** matchController */);
+router.post("/create", isAuth, MatchController.create);
 
 module.exports = router;
