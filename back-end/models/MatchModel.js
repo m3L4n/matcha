6146482ffd1837f42c74c3cd75daf0ab3dfb019a
macrespo42 <macrespo@student.42.fr>
2023-09-26 21:32:11 +0200

    feat: losse/win elo algorithm

--- back-end/models/MatchModel.js
+++ back-end/models/MatchModel.js
@@ -3,12 +3,40 @@ const db = require("../db/db");
 
 class MatchModel {
 
+  static async #updateElo(idUserToUpdate, idOpponent, actual_outcome) {
+    // TODO update user elo when liked
+    // RA current liked_user elo
+    // RB current liker user elo
+    // EA expected outcomes of the match (if like 1 if unlike 0 if same elo 0.5)
+    // SA actual outcomes of the match (if like 1 if unlike 0)
+    // K scaling factor (32)
+    // RA + K * (SA - EA)
+    let K = 32;
+    try {
+      const userToUpdate = await db.query("SELECT rate_fame from users WHERE id = $1", [idUserToUpdate]);
+      const opponent = await db.query("SELECT rate_fame from users WHERE id = $1", [idOpponent]);
+      let ra = userToUpdate.rows[0].rate_fame;
+      let rb = opponent.rows[0].rate_fame;
+      let expected_outcome = 0.5;
+      if (ra > rb) {
+        expected_outcome = 0;
+      } else if (ra < rb) {
+        expected_outcome = 1;
+      }
+      let updatedRateFame = ra + K * (actual_outcome * expected_outcome);
+      return db.query("UPDATE users SET rate_fame = $1 WHERE id = $2", [updatedRateFame, idUserToUpdate]);
+    } catch (error) {
+      throw error;
+    }
+  }
+
   /**
     * Create a like
     * @param {string} receiverId
     * @param {string} requesterId
   **/
   static createLike = (receiverId, requesterId) => {
+    this.#updateElo()
     return new Promise(next => {
       db.query('SELECT "like" FROM match WHERE id_receiver = $1', [requesterId])
         .then(result => {
@@ -38,7 +66,7 @@ class MatchModel {
       db.query('SELECT "like" FROM match WHERE id_requester = $1', [requesterId])
         .then(result => {
           if (result.rows.length > 0) {
-            db.query('UPDATE match SET "like" = true \
+            db.query('UPDATE match SET "like" = false \
               WHERE id_requester = $1 AND id_receiver = $2', [requesterId, receiverId])
               .then(result => next(result))
               .catch(error => next(error))
