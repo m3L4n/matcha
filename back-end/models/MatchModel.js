e6be203cf39fd714070218f552bca9023d61f1d3
macrespo42 <macrespo@student.42.fr>
2023-09-27 13:58:04 +0200

    feat: update elo when user liked/unliked

--- back-end/models/MatchModel.js
+++ back-end/models/MatchModel.js
@@ -3,31 +3,30 @@ const db = require("../db/db");
 
 class MatchModel {
 
-  static async #updateElo(idUserToUpdate, idOpponent, actual_outcome) {
-    // TODO update user elo when liked
-    // RA current liked_user elo
-    // RB current liker user elo
-    // EA expected outcomes of the match (if like 1 if unlike 0 if same elo 0.5)
-    // SA actual outcomes of the match (if like 1 if unlike 0)
-    // K scaling factor (32)
-    // RA + K * (SA - EA)
-    let K = 32;
-    try {
-      const userToUpdate = await db.query("SELECT rate_fame from users WHERE id = $1", [idUserToUpdate]);
-      const opponent = await db.query("SELECT rate_fame from users WHERE id = $1", [idOpponent]);
-      let ra = userToUpdate.rows[0].rate_fame;
-      let rb = opponent.rows[0].rate_fame;
-      let expected_outcome = 0.5;
-      if (ra > rb) {
-        expected_outcome = 0;
-      } else if (ra < rb) {
-        expected_outcome = 1;
-      }
-      let updatedRateFame = ra + K * (actual_outcome * expected_outcome);
-      return db.query("UPDATE users SET rate_fame = $1 WHERE id = $2", [updatedRateFame, idUserToUpdate]);
-    } catch (error) {
-      throw error;
-    }
+  static #updateElo(idPlayer, idOpponent, actual_outcome) {
+    const K = 32;
+    return new Promise(next => {
+      db.query("SELECT rate_fame from users WHERE id = $1", [idPlayer])
+        .then(userToUpdate => {
+          const currentUserRating = userToUpdate.rows[0].rate_fame;
+          db.query("SELECT rate_fame from users WHERE id = $1", [idOpponent])
+            .then(opponent => {
+              const opponentRating = opponent.rows[0].rate_fame;
+              let expected_outcome = 0.5;
+              if (currentUserRating > opponentRating) {
+                expected_outcome = 0;
+              } else if (currentUserRating < opponentRating) {
+                expected_outcome = 1;
+              }
+              let updatedRateFame = currentUserRating + K * (actual_outcome - expected_outcome);
+              db.query("UPDATE users SET rate_fame = $1 WHERE id = $2", [updatedRateFame, idOpponent])
+                .then(result => next(result))
+                .catch(error => next(error))
+            })
+            .catch(error => next(error))
+        })
+        .catch(error => next(error))
+    })
   }
 
   /**
@@ -36,21 +35,24 @@ class MatchModel {
     * @param {string} requesterId
   **/
   static createLike = (receiverId, requesterId) => {
-    this.#updateElo()
     return new Promise(next => {
       db.query('SELECT "like" FROM match WHERE id_receiver = $1', [requesterId])
         .then(result => {
-          if (!result.rows.length) {
-            db.query('INSERT INTO match("like", block, id_requester, id_receiver)  \
-              VALUES(false, false, $1, $2)', [requesterId, receiverId])
-              .then(result => next(result))
-              .catch(error => next(error))
-          } else {
-            db.query('UPDATE match SET "like" = true \
-              WHERE id_requester = $1 AND id_receiver = $2', [receiverId, requesterId])
-              .then(result => next(result))
-              .catch(error => next(error))
-          }
+          this.#updateElo(receiverId, requesterId, 1)
+            .then(() => {
+              if (!result.rows.length) {
+                db.query('INSERT INTO match("like", block, id_requester, id_receiver)  \
+                VALUES(false, false, $1, $2)', [requesterId, receiverId])
+                  .then(result => next(result))
+                  .catch(error => next(error))
+              } else {
+                db.query('UPDATE match SET "like" = true \
+                WHERE id_requester = $1 AND id_receiver = $2', [receiverId, requesterId])
+                  .then(result => next(result))
+                  .catch(error => next(error))
+              }
+            })
+            .catch(error => next(error))
         })
         .catch(error => next(error))
     });
@@ -65,14 +67,18 @@ class MatchModel {
     return new Promise(next => {
       db.query('SELECT "like" FROM match WHERE id_requester = $1', [requesterId])
         .then(result => {
-          if (result.rows.length > 0) {
-            db.query('UPDATE match SET "like" = false \
+          this.#updateElo(receiverId, requesterId, 0)
+            .then(() => {
+              if (result.rows.length > 0) {
+                db.query('UPDATE match SET "like" = false \
               WHERE id_requester = $1 AND id_receiver = $2', [requesterId, receiverId])
-              .then(result => next(result))
-              .catch(error => next(error))
-          } else {
-            next(result)
-          }
+                  .then(result => next(result))
+                  .catch(error => next(error))
+              } else {
+                next(result)
+              }
+            })
+            .catch(error => next(error))
         })
         .catch(error => next(error))
     })
