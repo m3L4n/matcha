6207b43e3bb97852b5b1f6ca224e34eecfe50341
macrespo42 <macrespo@student.42.fr>
2023-10-15 16:28:44 +0200

    feat: list all conversations in picker - add fetchConversation - rework on conversation integration

--- front-end/src/components/Message/ConversationPicker.jsx
+++ front-end/src/components/Message/ConversationPicker.jsx
@@ -4,21 +4,34 @@ import dummy from "./dummy-data.json";
 import { useState } from "react";
 import Message from "./Message/Message";
 import { useMedia } from "react-media";
+import getConversations from "./fetchConversations";
+import { useQuery } from "@tanstack/react-query";
 
 const ConversationPicker = () => {
   const [conversationPicker, setConversationPicker] = useState(true);
   const [conversationId, setConversationId] = useState("");
   const isMobile = useMedia({ query: "(max-width: 1259px)" });
-  const conversations = dummy.map(conversation => (
+
+  const { status, error, data } = useQuery({
+    queryKey: ["conversations"],
+    queryFn: getConversations
+  });
+
+  if (status === "error") {
+    conversations = [];
+  }
+
+  let conversations = data?.result?.rows?.map(conversation => (
     <Conversation
-      key={conversation.id}
-      id={conversation.id}
-      firstName={conversation.firstName}
-      lastMessage={conversation.lastMessage}
+      key={conversation.conversation_id}
+      id={conversation.conversation_id}
+      firstName={conversation.chat_partner_name}
+      lastMessage={conversation.last_message ? conversation.last_message : ""}
       setConversationPicker={setConversationPicker}
       setConversationId={setConversationId}
     />
   ));
+
   return (
     <section>
       <h1 className="header">mat-chat</h1>
---
+++ front-end/src/components/Message/fetchConversations.js
@@ -0,0 +1,20 @@
+async function getConversations() {
+  const url = `${import.meta.env.VITE_BACKEND_API_URL}/conversation`;
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8"
+    },
+    credentials: "include"
+  };
+
+  const response = await fetch(url, options);
+
+  if (!response) {
+    throw new Error("Can't get conversations");
+  }
+
+  return response.json();
+}
+
+export default getConversations;
