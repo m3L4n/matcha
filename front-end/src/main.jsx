60146ccb40faf8b8bd36ddb6ab8bd4fe46d78d0d
mpochard <88907375+m3L4n@users.noreply.github.com>
2023-10-17 11:59:50 +0200

    feat: add

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -7,7 +7,6 @@ import { AuthProvider } from "./Context/AuthContext";
 import "./index.css";
 import { router } from "./Router/Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./app";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,7 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <App />
+        <RouterProvider router={router} />
         <ToastContainer />
       </AuthProvider>
     </QueryClientProvider>
