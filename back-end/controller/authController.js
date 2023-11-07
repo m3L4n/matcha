5793165430b4128f78b208377718e26754a82776
mpochard <88907375+m3L4n@users.noreply.github.com>
2023-11-07 14:44:20 +0100

    feat: discord implemented

--- back-end/controller/authController.js
+++ back-end/controller/authController.js
@@ -6,27 +6,35 @@ class authContrroller {
     params.append("client_secret", process.env.CLIENT_SECRET);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
+    params.append("scope", "identify, email");
     params.append("redirect_uri", process.env.REDIRECT_URL);
     try {
       const options = {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
+          "Content-type": "application/x-www-form-urlencoded",
         },
         body: params,
       };
+      console.log(params);
+      const response = await fetch("https://discord.com/api/oauth2/token", options);
+      const responseJSON = await response.json();
+      console.log(" coucou c'est la response test", responseJSON);
+      const { access_token, token_type } = responseJSON;
       const options1 = {
         method: "GET",
         headers: {
+          "Content-type": "application/x-www-form-urlencoded",
           authorization: `${token_type} ${access_token}`,
         },
       };
-      const response = await fetch("https://discord.com/api/oauth2/token", options);
-      const { access_token, token_type } = response.data;
-      const userDataResponse = await fetch("https://discord.com/api/users/@me", options);
-      console.log("Data: ", userDataResponse.data);
-      console.log(response);
-    } catch (error) {}
+      const userDataResponse = await fetch("https://discord.com/api/users/@me", options1);
+      const userDataResponseJSON = await userDataResponse.json();
+      console.log("Data: ", userDataResponseJSON);
+      return res.status(200).json({ status: 200, result: userDataResponseJSON });
+    } catch (error) {
+      return res.status(404).json({ status: 404, msg: "cant connect with discord" });
+    }
   };
 }
 module.exports = {
