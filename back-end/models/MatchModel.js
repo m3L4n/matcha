e41613c87bc010d0ae5e5e3800903d34e98fe427
macrespo42 <macrespo@student.42.fr>
2023-11-11 16:12:55 +0100

    fix: elo updating system formula correct Ea / Eb value merge 2 update requests in only one improve log counting in seed

--- back-end/models/MatchModel.js
+++ back-end/models/MatchModel.js
@@ -1,9 +1,9 @@
 const db = require("../db/db");
-const { error } = require("../modules/response");
 
 class MatchModel {
   static #updateElo(idPlayer, idOpponent, actual_outcome = 1) {
     const K = 32;
+    const C = 400;
     return new Promise((next) => {
       db.query("SELECT rate_fame from users WHERE id = $1", [idPlayer])
         .then((userToUpdate) => {
@@ -11,31 +11,40 @@ class MatchModel {
           db.query("SELECT rate_fame from users WHERE id = $1", [idOpponent])
             .then((opponent) => {
               const opponentRating = opponent.rows[0].rate_fame;
-              let expected_outcome = 0.5;
-              if (currentUserRating > opponentRating) {
-                expected_outcome = 0;
-                actual_outcome = 0;
-              } else if (currentUserRating < opponentRating) {
-                expected_outcome = 1;
-              }
-              const updatedRateFame =
-                currentUserRating + K * (actual_outcome - expected_outcome);
-              db.query(
-                "UPDATE users SET rate_fame = $1 WHERE id = $2 \
-                RETURNING *",
-                [updatedRateFame, idPlayer],
-              )
-                .then(() => {
-                  const fameCost =
-                    opponentRating + K * (0 - (expected_outcome === 1 ? 0 : 1));
-                  db.query(
-                    "UPDATE users SET rate_fame = $1 WHERE id = $2 RETURNING *",
-                    [fameCost, idOpponent],
-                  )
-                    .then((result) => next(result))
-                    .catch((error) => next(error));
-                })
+              const qa = Math.pow(currentUserRating / C, 10);
+              const qb = Math.pow(opponentRating / C, 10);
+              const winnerOutcome = qa / (qa + qb);
+              const loserOutcome = qb / (qb + qa);
+              const opponentOutcome = actual_outcome === 1 ? 0 : 1;
 
+              const playerRateFame = Math.ceil(
+                currentUserRating + K * (actual_outcome - winnerOutcome),
+              );
+
+              const opponentRateFame = Math.ceil(
+                opponentRating + K * (opponentOutcome - loserOutcome),
+              );
+
+              if (playerRateFame < 0) {
+                playerRateFame = 0;
+              }
+
+              if (opponentRateFame < 0) {
+                opponentRateFame = 0;
+              }
+
+              db.query(
+                "UPDATE users SET \
+                rate_fame = \
+                CASE id\
+                  WHEN $1 THEN CAST($2 AS INTEGER)\
+                  WHEN $3 THEN CAST($4 AS INTEGER)\
+                END\
+                WHERE id IN ($1, $3)\
+                RETURNING *",
+                [idPlayer, playerRateFame, idOpponent, opponentRateFame],
+              )
+                .then((result) => next(result))
                 .catch((error) => next(error));
             })
             .catch((error) => next(error));
@@ -43,6 +52,7 @@ class MatchModel {
         .catch((error) => next(error));
     });
   }
+
   static #deleteMatch = (id_requester, id_receiver) => {
     return new Promise((next) => {
       db.query(
@@ -50,11 +60,12 @@ class MatchModel {
         [id_requester, id_receiver],
       )
         .then((data) => {
-          return next(error);
+          return next(data);
         })
         .catch((error) => next(error));
     });
   };
+
   static update(id_requester, id_receiver, like, block) {
     return new Promise((next) => {
       db.query(
@@ -67,65 +78,6 @@ class MatchModel {
         .catch((error) => next(error));
     });
   }
-
-  /**
-   * create a new match with paramter modifiable
-   * @param {String} id_requester
-   * @param {String} id_receiver
-   * @param {boolean} like //
-   * @param {boolean} block
-   */
-  static #createMatch = (
-    id_requester,
-    id_receiver,
-    like = false,
-    block = false,
-  ) => {
-    return new Promise((next) => {
-      db.query(
-        `SELECT * FROM match WHERE id_requester = $1 AND id_receiver = $2 OR id_requester = $2 AND id_receiver = $1`,
-        [id_requester, id_receiver],
-      )
-        .then((match) => {
-          if (match.rowCount > 0) {
-            this.update(id_requester, id_receiver, like, block)
-              .then(() => {
-                let elo = 0;
-                if (like == true || block == true) {
-                  elo = 1;
-                }
-                this.#updateElo(id_requester, id_receiver, elo)
-                  .then((eloUpdate) => next(eloUpdate))
-                  .catch((error) => next(error));
-              })
-              .catch((error) => next(error));
-          } else {
-            db.query(
-              `INSERT INTO match (id_requester, id_receiver, "like", "block") \
-      VALUES ($1, $2, $3, $4) RETURNING *`,
-              [id_requester, id_receiver, like, block],
-            )
-              .then((dataInserted) => {
-                if (dataInserted.rowCount > 0) {
-                  let elo = 0;
-                  if (like == true || block == true) {
-                    elo = 1;
-                  }
-                  this.#updateElo(id_requester, id_receiver, elo)
-                    .then((eloUpdate) => next(eloUpdate))
-                    .catch((error) => next(error));
-                } else {
-                  const error = new Error("cant create new match");
-                  error.status = 500;
-                  return next(error);
-                }
-              })
-              .catch((error) => next(error));
-          }
-        })
-        .catch((error) => next(error));
-    });
-  };
 
   static getRelationShip = async (requesterId, receiverId) => {
     try {
@@ -149,7 +101,6 @@ class MatchModel {
         'SELECT  match.id_requester, match."like" FROM match  WHERE match.id_requester = $1 AND match.id_receiver= $2 ',
         [requesterId, receiverId],
       );
-      // console.log(responseRequester);
       if (responseRequester.rowCount > 0) {
         const obj = JSON.parse(JSON.stringify(responseRequester.rows[0]));
 
@@ -167,7 +118,6 @@ class MatchModel {
           'SELECT  match.id_requester, match."like" FROM match  WHERE match.id_requester = $2 AND match.id_receiver= $1 ',
           [requesterId, receiverId],
         );
-        // console.log("response receiver", responseReceiver);
         if (responseReceiver.rowCount == 1) {
           const obj = JSON.parse(JSON.stringify(responseReceiver.rows[0]));
           if (responseReceiver.rows[0].like) {
@@ -272,51 +222,46 @@ class MatchModel {
         [requesterId, receiverId],
       )
         .then((result) => {
-          this.#updateElo(receiverId, requesterId, 0)
-            .then(() => {
-              if (result.rowCount > 0) {
-                if (result.rows[0].like) {
-                  const { id_requester, id_receiver } = result.rows[0];
-                  if (
-                    (id_requester == requesterId &&
-                      id_receiver == receiverId) ||
-                    (id_requester == receiverId && id_receiver == requesterId)
-                  ) {
-                    db.query(
-                      'UPDATE match\
+          if (result.rowCount > 0) {
+            if (result.rows[0].like) {
+              const { id_requester, id_receiver } = result.rows[0];
+              if (
+                (id_requester == requesterId && id_receiver == receiverId) ||
+                (id_requester == receiverId && id_receiver == requesterId)
+              ) {
+                db.query(
+                  'UPDATE match\
                         SET "like" = false,\
                         id_receiver = $1,\
                         id_requester = $2',
-                      [requesterId, receiverId],
-                    )
-                      .then((result) => next(result))
-                      .catch((error) => next(error));
-                  } else {
-                    db.query(
-                      'UPDATE match SET "like" = false \
+                  [requesterId, receiverId],
+                )
+                  .then((result) => next(result))
+                  .catch((error) => next(error));
+              } else {
+                db.query(
+                  'UPDATE match SET "like" = false \
                     WHERE id_requester = $1 AND id_receiver = $2\
                     RETURNING *',
-                      [requesterId, receiverId],
-                    )
-                      .then((result) => next(result))
-                      .catch((error) => next(error));
-                  }
-                } else {
-                  db.query(
-                    "DELETE FROM match\
+                  [requesterId, receiverId],
+                )
+                  .then((result) => next(result))
+                  .catch((error) => next(error));
+              }
+            } else {
+              db.query(
+                "DELETE FROM match\
                     WHERE id_requester = $1 AND id_receiver = $2\
                     OR id_receiver = $1 AND id_requester = $2\
                     RETURNING *",
-                    [requesterId, receiverId],
-                  )
-                    .then((result) => next(result))
-                    .catch((error) => next(error));
-                }
-              } else {
-                next(result);
-              }
-            })
-            .catch((error) => next(error));
+                [requesterId, receiverId],
+              )
+                .then((result) => next(result))
+                .catch((error) => next(error));
+            }
+          } else {
+            next(result);
+          }
         })
         .catch((error) => next(error));
     });
